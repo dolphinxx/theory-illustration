d266f65df6d3af61ebb1892b0067c3ac1c88f170
dolphin <adaikiss@163.com>
2018-09-12 23:32:58 +0800

    expose params to UI interface

--- src/gc/index.js
+++ src/gc/index.js
@@ -4,8 +4,40 @@ const width = 500,
 const youngWidth = 400,youngHeight = 50,  youngX = 0, youngY = 0;
 const edenWidth = 200, survivorWidth = (youngWidth - edenWidth)/2;
 const tenuredX = 0, tenuredY = youngY + 100, tenuredWidth = 200;
+const splitterWidth = 2;
 
-const MaxTenuringThreshold = 5;
+let MaxTenuringThreshold = 5;
+let edenCollectChance = 0.8;
+let survivorCollectChance = 0.2;
+let objectBaseSize = 20;
+let tickPeriod = 500;
+
+let running = false;
+
+$(function(){
+    $('#edenCollectChance').val(edenCollectChance).change(function(){
+        edenCollectChance = Number($(this).val());
+    });
+    $('#survivorCollectChance').val(survivorCollectChance).change(function(){
+        survivorCollectChance = Number($(this).val());
+    });
+    $('#maxTenuringThreshold').val(MaxTenuringThreshold).change(function(){
+        MaxTenuringThreshold = Number($(this).val());
+    });
+    $('#objectBaseSize').val(objectBaseSize).change(function(){
+        objectBaseSize = Number($(this).val());
+    });
+    $('#tickPeriod').val(tickPeriod).change(function(){
+        tickPeriod = Number($(this).val());
+    });
+    $('#startBtn').click(function(){
+        running = !running;
+        $(this).text(running ? 'Stop':'Start');
+    });
+});
+function updateStatus(status) {
+    $('#status').text(status);
+}
 
 var svg = d3.select('#container')
     .attr("preserveAspectRatio", "xMidYMid")
@@ -26,20 +58,32 @@ const eden = svg.append('rect')
     .attr('width', edenWidth)
     .attr('height', youngHeight)
     .attr('class', 'eden');
-const survivor1 = svg.append('rect')
+svg.append('rect')
     .attr('x', youngX + edenWidth)
     .attr('y', youngY)
-    .attr('width', survivorWidth)
+    .attr('width', splitterWidth)
     .attr('height', youngHeight)
-    .attr('class', 'survivor');
-survivor1._data = {x: youngX + edenWidth, size: 0};
-const survivor2 = svg.append('rect')
-    .attr('x', youngX + edenWidth + survivorWidth)
+    .attr('class', 'splitter');
+const survivor1 = svg.append('rect')
+    .attr('x', youngX + edenWidth + splitterWidth)
     .attr('y', youngY)
     .attr('width', survivorWidth)
     .attr('height', youngHeight)
     .attr('class', 'survivor');
-survivor2._data = {x: youngX + edenWidth + survivorWidth, size: 0};
+survivor1._data = {x: youngX + edenWidth + splitterWidth, size: 0};
+svg.append('rect')
+    .attr('x', youngX + edenWidth + survivorWidth + splitterWidth)
+    .attr('y', youngY)
+    .attr('width', splitterWidth)
+    .attr('height', youngHeight)
+    .attr('class', 'splitter');
+const survivor2 = svg.append('rect')
+    .attr('x', youngX + edenWidth + survivorWidth + splitterWidth * 2)
+    .attr('y', youngY)
+    .attr('width', survivorWidth)
+    .attr('height', youngHeight)
+    .attr('class', 'survivor');
+survivor2._data = {x: youngX + edenWidth + survivorWidth + splitterWidth * 2, size: 0};
 svg.append('g')
     .attr('class', 'label-container')
     .append('text')
@@ -83,7 +127,7 @@ let edenObjects = [];
 let survivorObjects = [];
 let survivors = [survivor1, survivor2];
 function createObject() {
-    const size = Math.floor(Math.random() * 50 + 1);
+    const size = Math.ceil(Math.random() * objectBaseSize);
     const color = `#${Math.ceil((Math.random() * 9 + 1) * 20).toString(16)}${Math.ceil(Math.random() * 150 + 50).toString(16)}${Math.ceil(Math.random() * 150 + 50).toString(16)}`;
     const c = svg.append('svg')
         .attr('width', size)
@@ -103,15 +147,16 @@ function createObject() {
     c._data = {size};
     return c;
 }
-function isCollectable() {
-    return Math.random() > 0.7;
+function isCollectable(chance) {
+    return Math.random() < chance;
 }
 function minorGC() {
+    updateStatus('Minor GC start...');
     const toSurvivorObjects = [];
     for(let obj of [...edenObjects, ...survivorObjects]) {
         ticks.push(() => {
-            if(isCollectable()) {
-                collect(obj);
+            if(isCollectable(obj._data.position === 's'?survivorCollectChance:edenCollectChance)) {
+                collect(obj, edenObjects);
                 return;
             }
             const age = Number(obj.select('text').text());
@@ -120,9 +165,10 @@ function minorGC() {
                 return;
             }
             obj.select('text').text(String(age + 1));
+            obj._data.position = 's';
             toSurvivorObjects.push(obj);
             obj.transition()
-                .duration(500)
+                .duration(tickPeriod)
                 .attr('x', survivors[1]._data.x + survivors[1]._data.size);
             survivors[1]._data.size += obj._data.size;
         });
@@ -135,25 +181,25 @@ function minorGC() {
         survivors[0] = survivors[1];
         survivors[1] = oldFromSurvivor;
         edenSize = 0;
-        console.log('minor gc finished');
+        updateStatus('Minor GC finished.');
     });
 }
 function promote(obj) {
     removeFirst(edenObjects, obj);
     obj.transition()
-        .duration(500)
+        .duration(tickPeriod)
         .attr('x', tenuredX + tenuredGeneration._data.size)
         .attr('y', tenuredY);
     tenuredGeneration._data.size += obj._data.size;
 }
 
-function collect(obj) {
+function collect(obj, objects) {
     obj.select('rect').attr('fill', 'red');
     obj.transition()
-        .duration(500)
+        .duration(tickPeriod)
         .attr('width', 1)
         .on('end', () => {
-            removeFirst(edenObjects, obj);
+            removeFirst(objects, obj);
             obj.remove();
         });
 }
@@ -168,17 +214,20 @@ function removeFirst(array, item) {
 
 const ticks = [];
 (function doTick() {
-    if(ticks.length > 0 ) {
-        ticks.shift()();
-    } else {
-        doCreate();
+    if(running) {
+        if(ticks.length > 0 ) {
+            ticks.shift()();
+        } else {
+            doCreate();
+        }
     }
     setTimeout(() => {
         doTick();
-    }, 500);
+    }, tickPeriod);
 }());
 
 function doCreate() {
+    updateStatus('OK');
     const obj = createObject();
     const size = obj._data.size;
     if(edenSize + size > edenWidth) {
@@ -186,17 +235,16 @@ function doCreate() {
         ticks.push(() => {
             edenObjects.push(obj);
             obj.transition()
-                .duration(500)
+                .duration(tickPeriod)
                 .attr('x', edenSize)
                 .attr('y', youngY);
             edenSize += size;
-            console.log('new obj');
         });
         return;
     }
     edenObjects.push(obj);
     obj.transition()
-        .duration(500)
+        .duration(tickPeriod)
         .attr('x', edenSize)
         .attr('y', youngY);
     edenSize += size;
